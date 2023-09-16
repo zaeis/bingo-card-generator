bab9b368806a3a3eefc46d886e924a0e7007c471
Elsa <edinletir@gmail.com>
2023-09-16 16:33:48 +0100

    begin

--- bingo-card.js
+++ bingo-card.js
@@ -6,29 +6,61 @@ function initAll() {
     document.getElementById("reload").onclick = anotherCard;
     newCard();
   }
-  else{
+  else {
     alert("Your browser does not support this script.");
   }
 }
 
 function newCard() {
-  for(var i=0 ; i<24 ; i++){
-      setSquare(i);
-    }
+  for (var i = 0; i < 24; i++) {
+    setSquare(i);
+  }
 }
 
-function setSquare(thisSquare){
+function setSquare(thisSquare) {
   var currentSquare = "square" + thisSquare;
-  var colPlace = new Array(0,1,2,3,4,0,1,2,3,4,0,1,3,4,0,1,2,3,4,0,1,2,3,4);
+  var colPlace = new Array(0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4);
   var colBasis = colPlace[thisSquare] * 15;
   var newNum = colBasis + getNewNum() + 1;
 
-  do{
+  do {
     newNum = colBasis + getNewNum() + 1;
-  }while(usedNums[newNum]);
-  
+  } while (usedNums[newNum]);
+
   usedNums[newNum] = true;
   document.getElementById(currentSquare).innerHTML = newNum;
+
+  // JavaScript array
+
+  // var data = Array.prototype.map.call(document.querySelectorAll('#items tr'), function (tr) {
+  //   return Array.prototype.map.call(tr.querySelectorAll('td'), function (td) {
+  //     return td.innerHTML;
+  //   });
+  // });
+
+  var data = Array.prototype.map.call(document.querySelectorAll('#items tr'), function (tr) {
+    return Array.prototype.map.call(tr.querySelectorAll('td'), function (td) {
+      return td.innerHTML;
+    });
+  });
+
+
+  // Get the list container element
+  let list =
+    document.getElementById('myList');
+
+  // Create the unordered list element
+  //and set its inner HTML using map() and join()
+  // let ul = `<ul>${data.map(data =>
+  //   `<li>${data}</li>`).join('')}
+  //                 </ul>`;
+
+  let ul = `<ul>${data.map(data => data.map(x => `<li> ${x} </li>`).join('')
+  ).join('')}
+                  </ul>`;
+
+  // Set the inner HTML of the list container
+  list.innerHTML = ul;
 }
 
 function getNewNum() {
